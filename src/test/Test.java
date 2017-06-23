31383c39912f1c2b1aa55905e14a6d87772322cb
profolsen <olsenp@strose.edu>
2017-06-23 12:34:40 -0400

    cleaning up samples directory to include only those programs that are tests.

--- src/test/Test.java
+++ src/test/Test.java
@@ -8,14 +8,14 @@ import emulator.Emulator;
  */
 public class Test {
     public static void main(String[] args) {
-        AllInOne.Main(new String[]{"samples/program.txt", "256"});
-        AllInOne.Main(new String[]{"samples/jaltestasm.txt", "256"});
-        AllInOne.Main(new String[]{"samples/memtestasm.txt", "256"});
-        AllInOne.Main(new String[]{"samples/stringtestasm.txt", "256"});
-        AllInOne.Main(new String[]{"samples/errtestasm.txt", "256"});
-        AllInOne.Main(new String[]{"samples/includetestasm.txt", "256"});
+        AllInOne.Main(new String[]{"samples/program.ftnt", "256"});
+        AllInOne.Main(new String[]{"samples/jaltestasm.ftnt", "256"});
+        AllInOne.Main(new String[]{"samples/memtestasm.ftnt", "256"});
+        AllInOne.Main(new String[]{"samples/stringtestasm.ftnt", "256"});
+        AllInOne.Main(new String[]{"samples/errtestasm.ftnt", "256"});
+        AllInOne.Main(new String[]{"samples/includetestasm.ftnt", "256"});
         AllInOne.Main(new String[]{"samples/include_1.ftnt", "256"});
-        Emulator.main(new String[]{"samples/fibonacci.txt", "256"});
-        AllInOne.Main(new String[]{"samples/memtestbadasm.txt", "256"});
+        Emulator.main(new String[]{"samples/fibonacci.i", "256"});
+        AllInOne.Main(new String[]{"samples/memtestbadasm.ftnt", "256"});
     }
 }
