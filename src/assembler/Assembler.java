85999192fe7028e62f87dbceacef5aeaec110a64
profolsen <olsenp@strose.edu>
2017-06-24 13:58:41 -0400

    added legal information to each source file.

--- src/assembler/Assembler.java
+++ src/assembler/Assembler.java
@@ -4,8 +4,28 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-/**
- * Created by po917265 on 6/3/17.
+/*
+MIT License
+
+Copyright (c) 2017 Paul Olsen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
  */
 public class Assembler {
 
@@ -82,8 +102,7 @@ public class Assembler {
             } else if(program.get(i).startsWith("!")) {
                 program.set(i, program.get(i).substring(1));
                 Integer address = symbolTable.get(program.get(i));
-                if(address == null) {
-                    System.out.println("Undefined array name: @" + pc2line.get(pc2line.floorKey(i)));
+                if(address == null) {System.out.println("Undefined array name: @" + pc2line.get(pc2line.floorKey(i)));
                 } else {
                     program.set(i, "" + (program.size() + address));
                 }
